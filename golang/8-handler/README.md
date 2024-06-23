5818118600478961f3a4e7ef8119369519be4ba7
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-23 13:59:25 +0700

    setup8

--- golang/8-handler/README.md
+++ golang/8-handler/README.md
@@ -1,1 +1,9 @@
 # 8-handler
+
+# Running the project
+- copy `.env.template` to `.env`
+- run `docker-compose up -d` or `make docker` to start the database
+- run `go run cmd/main.go` or `make server` for normal run, `air` for hot reload
+
+# Handler
+`main.go` is a bit messy and has a lot of imperative code. We will move the imperative code to the handler to make `main.go` more declarative.
---
+++ golang/8-handler/cmd/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/isd-sgcu/onboarding-backend/golang/6-router/config"
+	"github.com/isd-sgcu/onboarding-backend/golang/6-router/database"
+	"github.com/isd-sgcu/onboarding-backend/golang/6-router/internal/dto"
+	"github.com/isd-sgcu/onboarding-backend/golang/6-router/internal/user"
+	"github.com/isd-sgcu/onboarding-backend/golang/6-router/router"
+)
+
+func main() {
+	conf, err := config.LoadConfig()
+	if err != nil {
+		panic(fmt.Sprintf("Failed to load config: %v", err))
+	}
+
+	db, err := database.InitDatabase(&conf.Database, conf.App.IsDevelopment())
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to database: %v", err))
+	}
+
+	userRepo := user.NewRepository(db)
+	userService := user.NewService(userRepo)
+
+	r := router.New(conf)
+	r.V1Post("/user", func(c router.Context) {
+		var createUserDto dto.CreaterUserRequest
+		if err := c.Bind(&createUserDto); err != nil {
+			c.JSON(400, err)
+			return
+		}
+
+		createdUser, err := userService.Create(&createUserDto)
+		if err != nil {
+			c.JSON(500, err)
+			return
+		}
+
+		c.JSON(200, createdUser)
+	})
+
+	r.V1Get("/user/:id", func(c router.Context) {
+		id := c.Param("id")
+		if id == "" {
+			c.JSON(400, "id is required in url param")
+			return
+		}
+
+		total, apperr := userService.FindOne(&dto.FindOneUserRequest{Id: id})
+		if apperr != nil {
+			c.JSON(500, apperr)
+			return
+		}
+
+		c.JSON(200, total)
+	})
+
+	r.V1Delete("/user/:id", func(c router.Context) {
+		id := c.Param("id")
+		if id == "" {
+			c.JSON(400, "id is required in url param")
+			return
+		}
+
+		total, apperr := userService.Delete(&dto.DeleteUserRequest{Id: id})
+		if apperr != nil {
+			c.JSON(500, apperr)
+			return
+		}
+
+		c.JSON(200, total)
+	})
+
+	if err := r.Run(fmt.Sprintf(":%v", conf.App.Port)); err != nil {
+		panic(fmt.Sprintf("Failed to run router: %v", err))
+	}
+}
